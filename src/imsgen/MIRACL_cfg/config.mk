5df39b1756fa0a1a634c8eba86cd90968b8e8cbb
Morgan Girling <girling_morgan@projectara.com>
2016-02-11 10:53:39 -0800

    Bootrom Tools: Fix "make install" error (post-review)
    Bootrom-tools "make install" ignored an (ignorable) build error in the
    MIRACL library. Ported Jun's makefile changes for building the MIRACL
    library from bootrom. This eliminated the error and properly integrated
    the MIRACL library into the bootrom-tools project.
    (Incorporates changes from Marti's review)
    
    Testing Done: Builds cleanly for "make" and "make install" (NB. "make
    install" must be run as superuser or the copies to /usr/local/bin fail).
    Bug Number: SW-2893
    Review URL: https://reviews.leaflabs.com/r/2918/
    Reviewed by:
    
    Signed-off-by:  Morgan Girling <morgang@bsquare.com>

--- src/imsgen/MIRACL_cfg/config.mk
+++ src/imsgen/MIRACL_cfg/config.mk
@@ -1,25 +1,25 @@
 # Short Weierstrass form curve
 MCL_WEIERSTRASS:=0
-# Edwards or Twisted Edwards curve  
+# Edwards or Twisted Edwards curve
 MCL_EDWARDS:=1
-# Montgomery form curve 
+# Montgomery form curve
 MCL_MONTGOMERY:=2
 
-# NIST 256-bit standard curve - MCL_WEIERSTRASS only 
+# NIST 256-bit standard curve - MCL_WEIERSTRASS only
 MCL_NIST256:=0
-# Bernstein's Modulus 2^255-19 - MCL_EDWARDS or MCL_MONTGOMERY only 
+# Bernstein's Modulus 2^255-19 - MCL_EDWARDS or MCL_MONTGOMERY only
 MCL_C25519:=1
 # Bernstein et al Curve41417 2^414-17 - MCL_EDWARDS only
-MCL_C41417:=2  
+MCL_C41417:=2
 # NIST 384-bit standard curve - MCL_WEIERSTRASS only
 MCL_NIST384:=3
 # NIST 521-bit standard curve - MCL_WEIERSTRASS only
 MCL_NIST521:=4
-# Goldilocks curve - MCL_EDWARDS only  
+# Goldilocks curve - MCL_EDWARDS only
 MCL_C448:=5
 
 # Default configuration
-# size of mcl_chunk in bits = wordlength of computer = 16, 32 or 64. 
+# size of mcl_chunk in bits = wordlength of computer = 16, 32 or 64.
 # Note not all curve options are supported on 16-bit processors - see rom.c
 MCL_CHUNK:=32
 # Current choice of Elliptic Curve (see mcl_arch.h)
@@ -27,10 +27,10 @@ MCL_CHUNK:=32
 # passed in from the make command line.
 MCL_CHOICE:=$(MCL_C448)
 
-# type of curve  (see mcl_arch.h) 
+# type of curve  (see mcl_arch.h)
 MCL_CURVETYPE:=$(MCL_EDWARDS)
-# 2^n multiplier of MCL_BIGBITS to specify supported Finite Field size, 
-# e.g 2048=256*2^3 where MCL_BIGBITS=256  (see mcl_config.h) 
+# 2^n multiplier of MCL_BIGBITS to specify supported Finite Field size,
+# e.g 2048=256*2^3 where MCL_BIGBITS=256  (see mcl_config.h)
 MCL_FFLEN:=8
 
 # Application specific configs to build multiple curves.
